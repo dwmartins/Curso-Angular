6ac88666df56936efe33a2f4d06d5ef7150ba381
Douglas Martins <douglas5422@outlook.com>
2022-12-12 14:04:09 -0300

    feat: intendendo o que é interpolation

---
+++ src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.scss']
+})
+export class DataBidingComponent implements OnInit {
+  
+  public nome: string = "Douglas";
+  public idade: number = 24;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}

ef34903e1647a58f50cd1a0715eeaad924778b45
Douglas Martins <douglas5422@outlook.com>
2022-12-16 10:37:52 -0300

    Entendendo o Property Binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -10,6 +10,10 @@ export class DataBidingComponent implements OnInit {
   public nome: string = "Douglas";
   public idade: number = 24;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__480.jpg";
+  public imgTitle: string = "Property Binding";
+
   constructor() {}
 
   ngOnInit(): void {
